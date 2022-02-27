b7c31198246239c50ae213cb524627a39da2674d
Rishika112001 <99496586+Rishika112001@users.noreply.github.com>
2022-02-27 21:47:52 +0530

    continue

---
+++ 10.dart
@@ -0,0 +1,15 @@
+void main() { //--------------------------------------------same label used as break
+	// CONTINUE keyword
+	// Using Labels
+
+	myLoop: for (int i = 1; i <= 3; i++) {
+
+		myInnerLoop: for (int j = 1; j <= 3; j++) {
+
+			if (i == 2 && j == 2) {
+				continue myLoop;
+			}
+			print("$i  $j");
+		}
+	}
+}
